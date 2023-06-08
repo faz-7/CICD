1f89fb0c96c1ae7260364ec84b2ffe0d4dac3da9
Faeze <112781265+faz-7@users.noreply.github.com>
2023-06-08 21:32:05 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
-def test_add_numbers():
-    assert add_numbers(1, 2) == 3
+import unittest
+from my_module import add_numbers
 
-def test_add_numbers_with_negative_numbers():
-    assert add_numbers(-1, 2) == 1
+class TestAddNumbers(unittest.TestCase):
+    def test_add_numbers_positive_numbers(self):
+        self.assertEqual(add_numbers(2, 3), 5)
 
-def test_add_numbers_with_zero():
-    assert add_numbers(0, 5) == 5
+    def test_add_numbers_negative_numbers(self):
+        self.assertEqual(add_numbers(-2, -3), -5)
 
-def test_add_numbers_with_large_numbers():
-    assert add_numbers(1000000, 1000000) == 2000000
+    def test_add_numbers_positive_and_negative_numbers(self):
+        self.assertEqual(add_numbers(-2, 3), 1)
+
+    def test_add_numbers_zero_numbers(self):
+        self.assertEqual(add_numbers(0, 0), 0)
